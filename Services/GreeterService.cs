e6751be457871d71f4ef6728f7ffc992921e1a9e
Vlad Timu <vlad.timu@smartboxdigital.com>
2020-12-09 18:41:31 +0200

    lab

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -7,20 +7,30 @@ using Microsoft.Extensions.Logging;
 
 namespace Timu_Vlad_Lab10
 {
-    public class GreeterService : Greeter.GreeterBase
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
+        return Task.FromResult(new HelloReply
         {
-            _logger = logger;
-        }
-
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(new HelloReply
-            {
-                Message = "Hello " + request.Name
-            });
-        }
+            Message = "Hello " + request.Name
+        });
+    }
+    public override Task<SResponse> SendStatus(SRequest request, ServerCallContext
+context)
+    {
+        List<StatusInfo> statusList = StatusRepo();
+        SResponse sRes = new SResponse();
+        sRes.StatusInfo.AddRange(statusList.Skip(request.No - 1).Take(1));
+        return Task.FromResult(sRes);
+    }
+    public List<StatusInfo> StatusRepo()
+    {
+        List<StatusInfo> statusList = new List<StatusInfo> {
+ new StatusInfo { Author = "Randy", Description = "Task 1 in progess"},
+ new StatusInfo { Author = "John", Description = "Task 1 just started"},
+ new StatusInfo { Author = "Miriam", Description = "Finished all tasks"},
+ new StatusInfo { Author = "Petra", Description = "Task 2 finished"},
+ new StatusInfo { Author = "Steve", Description = "Task 2 in progress"}
+ };
+        return statusList;
     }
 }
